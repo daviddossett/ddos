0cfaf4a97e68d8fed8eb910b40683c6e9c1bcbe5
David Dossett <ddossett@microsoft.com>
2025-11-22 22:30:06 -0800

    Add page title for dev

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,11 +2,14 @@ import Head from "next/head";
 import { Header } from "./header";
 
 function Layout({ children }: any): JSX.Element {
+  const isDev = process.env.NODE_ENV === "development";
+  const title = isDev ? "David Dossett — Dev" : "David Dossett";
+
   return (
     <div className="max-w-[960px] mx-auto w-full px-4">
       <Head>
-        <title>David Dossett</title>
-        <meta name="description" content="Product designer at Microsoft"/>
+        <title>{title}</title>
+        <meta name="description" content="Product designer at Microsoft" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
