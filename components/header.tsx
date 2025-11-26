d4033fd1ae80d4cded9c08b87ea2a812c8e5e815
David Dossett <daviddossett@github.com>
2025-11-26 11:10:58 -0800

    Fix header formatting

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,9 @@ import Link from "next/link";
 
 export const Header: React.FC = () => {
   return (
-    <header className="flex justify-between items-center py-8">
+    <header className="flex justify-between items-center pt-8">
       <h1 className="flex justify-items-center gap-2 text-base font-normal dark:text-slate-400">
-        <Link
-          className="text-base font-normal text-slate-900 dark:text-slate-400"
-          href="/"
-        >
+        <Link className="text-base font-normal text-slate-900 dark:text-slate-400" href="/">
           David Dossett
         </Link>
         <span className="text-2xl font-bold leading-3 text-blue-400">.</span>
